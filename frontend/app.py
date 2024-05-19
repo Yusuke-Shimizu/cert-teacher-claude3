2a449118892b81cc7a2cddb5d928a3995726efd2
Yusuke Shimizu <shimi.nekoze@gmail.com>
2024-05-19 10:14:51 +0900

    mod

--- frontend/app.py
+++ frontend/app.py
@@ -4,27 +4,37 @@ from botocore.exceptions import NoCredentialsError
 import os
 
 # 環境変数から必要な情報を取得
-bucket_name = os.getenv('BUCKET_NAME')
-table_name = os.getenv('DYNAMODB_TABLE_NAME')
+bucket_name = os.getenv("BUCKET_NAME")
+table_name = os.getenv("DYNAMODB_TABLE_NAME")
 
 # 必要な環境変数が設定されていない場合はエラーを表示して処理をここで停止
 if not bucket_name or not table_name:
-    missing_vars = ", ".join([var for var, value in [("BUCKET_NAME", bucket_name), ("DYNAMODB_TABLE_NAME", table_name)] if not value])
+    missing_vars = ", ".join(
+        [
+            var
+            for var, value in [
+                ("BUCKET_NAME", bucket_name),
+                ("DYNAMODB_TABLE_NAME", table_name),
+            ]
+            if not value
+        ]
+    )
     st.error(f"環境変数'{missing_vars}'が設定されていません。")
     st.stop()
 
 # DynamoDBテーブル名の確認
 # AWSリージョンを指定
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
 try:
     # テーブルが存在するか確認
     table = dynamodb.Table(table_name)
-    if table.table_status not in ['CREATING', 'UPDATING', 'DELETING', 'ACTIVE']:
+    if table.table_status not in ["CREATING", "UPDATING", "DELETING", "ACTIVE"]:
         st.error(f"DynamoDBテーブル'{table_name}'の状態が不正です。")
         st.stop()
 except dynamodb.meta.client.exceptions.ResourceNotFoundException:
     st.error(f"DynamoDBテーブル'{table_name}'が存在しません。")
     st.stop()
+
 
 # AWSリソースのクライアントを作成する関数
 def create_client(service_name):
@@ -34,9 +44,10 @@ def create_client(service_name):
         st.error(f"AWSの認証情報が見つかりません。")
         return None
 
+
 # S3にアップロードする関数
 def upload_to_s3(file, bucket_name, object_name):
-    s3_client = create_client('s3')
+    s3_client = create_client("s3")
     if s3_client is None:
         return False
     try:
@@ -46,48 +57,56 @@ def upload_to_s3(file, bucket_name, object_name):
         st.error(f"アップロード中にエラーが発生しました: {e}")
         return False
 
+
 # DynamoDBにクエリする関数
 def query_dynamodb(file_name_without_extension):
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(table_name)
     try:
-        response = table.get_item(Key={'id': file_name_without_extension})
-        return response.get('Item', None)
+        response = table.get_item(Key={"id": file_name_without_extension})
+        return response.get("Item", None)
     except Exception as e:
         st.error(f"DynamoDBからのクエリ中にエラーが発生しました: {e}")
         return None
 
+
 # ファイルアップロードウィジェットの作成
-uploaded_file = st.file_uploader("ファイルを選択してください", type=['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+uploaded_file = st.file_uploader(
+    "ファイルを選択してください", type=["txt", "pdf", "png", "jpg", "jpeg", "gif"]
+)
 
 # アップロードボタン
-if st.button('アップロード') and uploaded_file:
+if st.button("アップロード") and uploaded_file:
     file_name = uploaded_file.name
     if upload_to_s3(uploaded_file, bucket_name, file_name):
         st.success("ファイルが正常にアップロードされました。")
-        st.session_state['uploaded_file_name'] = file_name  # セッション状態にファイル名を保存
+        st.session_state["uploaded_file_name"] = (
+            file_name  # セッション状態にファイル名を保存
+        )
 
 # アップロードしたファイル名（拡張子を除く）をグローバル変数に保持
-# file_name_without_extension = os.path.splitext(st.session_state.get('uploaded_file_name', ''))[0] if 'uploaded_file_name' in st.session_state else None
-file_name_without_extension = os.path.splitext(uploaded_file.name)[0] if uploaded_file else None
+file_name_without_extension = (
+    os.path.splitext(uploaded_file.name)[0] if uploaded_file else None
+)
 
 # DynamoDBをクエリするボタンとその処理
-if st.button('問題を見る') and file_name_without_extension:
+if st.button("問題を見る") and file_name_without_extension:
     record = query_dynamodb(file_name_without_extension)
     if record:
-        st.session_state['japanese_question'] = record.get('japanese_question', '日本語の問題が見つかりません')
-        st.session_state['answer'] = record.get('answer', '回答が見つかりません')
+        st.session_state["japanese_question"] = record.get(
+            "japanese_question", "日本語の問題が見つかりません"
+        )
+        st.session_state["answer"] = record.get("answer", "回答が見つかりません")
         st.markdown(f"## 問題")
-        st.write(st.session_state['japanese_question'])
+        st.write(st.session_state["japanese_question"])
     else:
         st.error("指定されたIDのレコードが見つかりませんでした。")
 
 # 回答を見るボタン
-if st.button('回答を見る'):
-    if 'answer' in st.session_state and 'japanese_question' in st.session_state:
+if st.button("回答を見る"):
+    if "answer" in st.session_state and "japanese_question" in st.session_state:
         st.markdown(f"## 問題")
-        st.write(st.session_state['japanese_question'])
-        st.write(st.session_state['answer'])
+        st.write(st.session_state["japanese_question"])
+        st.write(st.session_state["answer"])
     else:
         st.error("問題を先に表示してください。")
-
--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ import invoke
 import logging
 import os
 import boto3
+import json
 
 logger = logging.getLogger(__name__)
 fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
@@ -20,24 +21,27 @@ def create_venv(c):
 def install(c):
     c.run("pip install -r requirements.txt -r requirements-dev.txt")
 
+
 @invoke.task
 def install_direnv(c):
     logging.info("install_direnv")
     # ファイルパスを指定
-    file_path = '/usr/local/bin/direnv'
-    
+    file_path = "/usr/local/bin/direnv"
+
     # ファイルが存在するかどうかを確認
     if not os.path.exists(file_path):
         logging.info(f"{file_path} ファイルが存在しないのでインストールを開始します。")
         bashrc_path = "~/.bashrc"
-        direnv_version="2.33.0" # Check https://github.com/direnv/direnv/releases
+        direnv_version = "2.33.0"  # Check https://github.com/direnv/direnv/releases
 
         # Install
         logging.info("Install direnv")
-        c.run(f"wget -O direnv https://github.com/direnv/direnv/releases/download/v{direnv_version}/direnv.linux-amd64")
+        c.run(
+            f"wget -O direnv https://github.com/direnv/direnv/releases/download/v{direnv_version}/direnv.linux-amd64"
+        )
         c.run("chmod +x direnv")
         c.run("sudo mv direnv /usr/local/bin/")
-        
+
         # Set bashrc
         logging.info("Set bashrc")
         c.run("echo 'eval \"$(direnv hook bash)\"' >> ~/.bashrc")
@@ -50,32 +54,55 @@ def install_direnv(c):
 
 @invoke.task
 def diff(c):
-    c.run("cdk diff", pty=True,)
+    c.run(
+        "cdk diff",
+        pty=True,
+    )
+
 
 @invoke.task
 def deploy(c):
     logging.info("deploy")
-    c.run("cdk deploy --require-approval never", pty=True,)
+    c.run(
+        "cdk deploy --require-approval never",
+        pty=True,
+    )
     logging.info("finish")
+
 
 @invoke.task
 def destroy(c):
     logging.info("destroy")
-    c.run("cdk destroy", pty=True,)
+    c.run(
+        "cdk destroy",
+        pty=True,
+    )
     logging.info("finish")
+
 
 @invoke.task
 def hotswap(c):
     logging.info("hotswap deploy")
-    c.run("cdk deploy --require-approval never --hotswap", pty=True,)
+    c.run(
+        "cdk deploy --require-approval never --hotswap",
+        pty=True,
+    )
     logging.info("finish")
+
 
 @invoke.task
 def front(c):
     logging.info("start frontend")
-    c.run("streamlit run frontend/app.py --logger.level=debug", pty=True,)
+    c.run(
+        "streamlit run frontend/app.py --logger.level=debug",
+        pty=True,
+    )
     logging.info("finish")
+
 
 @invoke.task
 def allow(c):
-    c.run("direnv allow", pty=True,)
+    c.run(
+        "direnv allow",
+        pty=True,
+    )
